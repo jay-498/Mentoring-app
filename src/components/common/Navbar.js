f9c05c1d2ba176a185a833f422878e45fe09fdfc
Ajay pediredla <57003507+jay-498@users.noreply.github.com>
2022-04-12 16:13:25 +0530

    navbar-responsive

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -8,15 +8,18 @@ export default function Navbar({ fixed }) {
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
     <>
-      <nav className="relative flex flex-wrap mx-20 items-center justify-between py-3">
-        <div className="flex flex-row items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+      <nav className="relative flex flex-wrap md:mx-20 mx-10 items-center justify-between py-3">
+        <div className="flex flex-row w-full xl:w-0 items-center mr-auto">
+          <div className="w-full relative flex justify-between items-center lg:w-auto lg:static lg:block lg:justify-start">
+            <div>
             <a
               className="text-[28px] font-semibold font-poppins leading-relaxed inline-block mr-4 py-2 whitespace-nowrap  text-gray"
               href="#pablo"
             >
               Company Name
             </a>
+            </div>
+            <div>
             <button
               className="cursor-pointer text-xl leading-none px-3 py-1 rounded block lg:hidden outline-none focus:outline-none"
               type="button"
@@ -24,15 +27,17 @@ export default function Navbar({ fixed }) {
             >
               <img src={navbarOpen?menuclose:menuclose} alt="menuopen"/>
             </button>
+            </div>
           </div>
+        </div>
           <div
             className={
-              "lg:flex flex-grow justify-start" +
+              "lg:flex items-center justify-start xs:flex-col " +
               (navbarOpen ? " flex" : " hidden")
             }
             id="example-navbar-danger"
           >
-            <ul className="flex flex-col lg:flex-row list-none lg:pl-52 gap-x-8">
+            <ul className="flex flex-col md:flex-row list-none xl:ml-auto gap-x-8">
               <li className="nav-item">
                 <a
                   className="px-3 py-2 flex items-center text-[18px] leading-snug text-gray"
@@ -58,22 +63,21 @@ export default function Navbar({ fixed }) {
                 </a>
               </li>
             </ul>
-          </div>
-        </div>
-        <div className={"lg:flex rounded my-4" +(navbarOpen ? " flex" : " hidden")} id="example-navbar-danger">
-          <div className="hidden rounded relative lg:ml-auto md:mr-0 md:block">
-            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-              <svg style={{transform: 'rotate(90deg)'}} className="w-5 h-5 text-[#999FAE]" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+            <div className={"lg:flex rounded my-4 pl-10 hidden" +(navbarOpen ? " flex" : " hidden")} id="example-navbar-danger">
+              <div className="hidden rounded relative  md:mr-0 md:block">
+                <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                  <svg style={{transform: 'rotate(90deg)'}} className="w-5 h-5 text-[#999FAE]" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+                </div>
+                <input type="text" id="email-adress-icon" className="block p-2 pl-10 w-[417px] outline-none h-10 text-[#999FAE] sm:text-sm 
+                    dark:placeholder-[#999FAE] rounded-md" placeholder="Search..." style={{backgroundColor:"rgba(114, 114, 114,0.1)"}}/>
+              </div>
+              <div className="hidden relative lg:ml-6 md:mr-0 md:block">
+                <button className="bg-[#8F6EC5] font-Helvetica text-white font-bold py-2 px-4 rounded-[5px]">
+                  Sign Up
+                </button>
+              </div>
             </div>
-            <input type="text" id="email-adress-icon" className="block p-2 pl-10 w-[417px] outline-none h-10 text-[#999FAE] sm:text-sm 
-                dark:placeholder-[#999FAE] rounded-md" placeholder="Search..." style={{backgroundColor:"rgba(114, 114, 114,0.1)"}}/>
           </div>
-          <div className="hidden relative lg:ml-6 md:mr-0 md:block">
-            <button className="bg-[#8F6EC5] font-Helvetica text-white font-bold py-2 px-4 rounded-[5px]">
-              Sign Up
-            </button>
-          </div>
-        </div>
       </nav>
     </>
   );
